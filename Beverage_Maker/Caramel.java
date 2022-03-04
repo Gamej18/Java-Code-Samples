e724dba58d7899de720fb6f6cab2f227c532898f
Gamej18 <78334342+Gamej18@users.noreply.github.com>
2022-03-04 13:43:30 -0600

    Add files via upload

---
+++ Beverage_Maker/Caramel.java
@@ -0,0 +1,10 @@
+public class Caramel extends Beverage {
+    public Caramel() {
+        description = "Caramel Coffee";
+    }
+
+    public double cost()
+    {
+        return 1.35;
+    }
+}
---
+++ Beverage_Maker/Hazelnut.java
@@ -0,0 +1,9 @@
+public class Hazelnut extends Beverage {
+    public Hazelnut() {
+        description = "Hazelnut Coffee";
+    }
+
+    public double cost() {
+        return 1.25;
+    }
+}
